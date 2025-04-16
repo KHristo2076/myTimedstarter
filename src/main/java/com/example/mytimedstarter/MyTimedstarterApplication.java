f2e6ef1ac58f3130384c78157a36d613e8b5b914
Christo <xristo.boton@yandex.ru>
2025-04-16 08:06:59 +0300

    commit for main branch

---
+++ src/main/java/com/example/mytimedstarter/MyTimedstarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytimedstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTimedstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTimedstarterApplication.class, args);
+	}
+
+}
